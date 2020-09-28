80142fa54ac18fa4f71c7168ebf2ba715f31a6df
woojin.kang <linecorp.com>
2020-09-28 22:25:34 +0900

    Revert "Add week04"
    This reverts commit d70fdcaf7373c9311560901f372fe2d5eb2206a2.

--- src/test/java/com/github/bakery/cesco/week04/Week04Test.java
+++ src/test/java/com/github/bakery/cesco/week04/Week04Test.java
@@ -1,40 +1,13 @@
 package com.github.bakery.cesco.week04;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
-@AutoConfigureMockMvc
-@SpringBootTest
-class Week04Test {
-    @Autowired
-    private MockMvc mockMvc;
+public class Week04Test {
 
     @Test
-    void LocalMapSecurityContextRepositoryTest() throws Exception {
-        mockMvc.perform(get("/week04/find").queryParam("username", "woojin").with(user("woojin")).contentType(MediaType.APPLICATION_JSON))
-               .andDo(print())
-               .andExpect(status().isOk());
+    void LocalMapSecurityContextRepositoryTest() {
         assertThat(LocalMapSecurityContextRepository.securityContexts).isNotEmpty();
-    }
-
-
-    @Test
-    void headerWriterTest() throws Exception {
-        mockMvc.perform(get("/week04/find").queryParam("username", "woojin").with(user("woojin")).contentType(MediaType.APPLICATION_JSON))
-               .andDo(print())
-               .andExpect(status().isOk())
-        .andExpect(header().exists("woojin"));
     }
 }
